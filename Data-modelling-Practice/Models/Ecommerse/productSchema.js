3eb338bbb6e0c54bee79c986d5b0025b02b95c49
Jatin Foujdar <jatin321001@gmail.com>
2023-11-03 15:16:38 +0530

    product schema commit

--- Data-modelling-Practice/Models/Ecommerse/productSchema.js
+++ Data-modelling-Practice/Models/Ecommerse/productSchema.js
@@ -1,6 +1,37 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({},{timestamps: true})
+const productSchema = new mongoose.Schema({
+
+    description:{
+        type: String,
+        required: true
+    },
+    name:{
+        type: String,
+        required: true
+    },
+    productImages:{
+        type: String
+    },
+    price:{
+        type: Number,
+        default: 0
+    },
+    stock:{
+        default: 0,
+        type: Number
+    },
+    category:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Category",
+        required: true,
+    },
+    owner:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    
+},{timestamps: true})
 
 
 export const Product = mongoose.model("Product", productSchema)
