cdf4d34804ebc15cadc0b319bd0f6d4dc2234cda
Jatin Foujdar <jatin321001@gmail.com>
2024-07-05 14:54:15 +0530

    pagi commit

--- pagination/vite-project/src/App.jsx
+++ pagination/vite-project/src/App.jsx
@@ -4,10 +4,11 @@ import "./index.css"
 function App() {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
 
   const fetchProduct = async () => {
     try {
-      const res = await fetch("https://dummyjson.com/products?limit=20");
+      const res = await fetch("https://dummyjson.com/products?limit=100");
       if (!res.ok) {
         throw new Error("Failed to fetch products");
       }
@@ -34,7 +35,7 @@ function App() {
       ) : (
         <div className="products">
           {products.length > 0 &&
-            products.map((prod) => (
+            products.slice(page*10 -10,page*10).map((prod) => (
               <div className="products__single" key={prod.id}>
                 <img src={prod.thumbnail} alt={prod.title} />
                 <span>{prod.title}</span>
@@ -42,6 +43,18 @@ function App() {
             ))}
         </div>
       )}
+      {
+        products.length>0 && <div className="pagination">
+          <span>⬅️</span>
+          {
+            [...Array(products.length/10)].map((_,i)=>{
+              return <span key={i}>{i+1}</span>
+            })
+          }
+          <span>1</span>
+          <span>⬅️</span>
+          </div>
+      }
     </div>
   );
 }
