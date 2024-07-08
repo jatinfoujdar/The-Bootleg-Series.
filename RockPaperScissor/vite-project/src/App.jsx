386636aa93cf344c42c296eb6f43753d12e8f028
Jatin Foujdar <jatin321001@gmail.com>
2024-07-08 11:44:03 +0530

    first init commit

---
+++ RockPaperScissor/vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import Game from "./components/Game"
+
+
+function App() {
+
+
+  return (
+    <>
+    <div style={{fontWeight: "bold"}}>Rock Paper Scissors</div>
+     <Game/>
+    </>
+  )
+}
+
+export default App
