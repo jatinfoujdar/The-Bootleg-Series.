041051b52b9e1f2ad61a176775a482b01f73c732
Jatin Foujdar <jatin321001@gmail.com>
2024-07-29 23:20:18 +0530

    delete todo commit

--- todoList/src/component/Todo.jsx
+++ todoList/src/component/Todo.jsx
@@ -1,44 +1,51 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { logo } from '../constant';
 
 const Todo = () => {
-    const [todos, setTodos] = useState([]);
-    const [newTodos, setNewTodos] = useState('');
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
 
+  const addTodo = () => {
+    if (newTodo.trim() !== '') {
+      setTodos([...todos, { text: newTodo, id: Date.now() }]);
+      setNewTodo('');
+    }
+  };
 
-    const addTodo = ()=>{
-        if(newTodos != ''){
-            setTodos([...todos,{text: newTodos, id: Date.now()}]);
-            setNewTodos('');
-        }
-    }
-    const deleteTodo = ()=>{
-        setTodos(todos.filter(todo => todo.id != id));
-    }
+  const deleteTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
   return (
-    <div >
-        <div className='flex items-center font-bold text-lg'>
-      <img className='w-5 mr-2' src={logo} alt="Logo" />
-      Todo List
-    </div>
-    <div className='mb-4'>
-        <input type='text' value={newTodos} onChange={(e)=> setNewTodos(e.target.value)} 
-        className="border p-2 mr-2 rounded"
+    <div>
+      <div className='flex items-center font-bold text-lg'>
+        <img className='w-5 mr-2' src={logo} alt="Logo" />
+        Todo List
+      </div>
+      <div className='mb-4'>
+        <input
+          type='text'
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          className="border p-2 mr-2 rounded"
           placeholder="Add a new task"
         />
-         <button onClick={addTodo} className="bg-blue-500 text-white p-2 rounded">
+        <button onClick={addTodo} className="bg-blue-500 text-white p-2 rounded">
           Add
         </button>
-        
-
-    </div>
-    <ul>
+      </div>
+      <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.text}</li>
+          <li key={todo.id} className="flex items-center">
+            <span className="flex-grow">{todo.text}</span>
+            <button onClick={() => deleteTodo(todo.id)} className="ml-2 bg-red-500 text-white p-1 rounded">
+              Delete
+            </button>
+          </li>
         ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Todo
+export default Todo;
