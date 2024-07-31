216e11541c8f18f459eb9fff48f1a1a432a4e241
Jatin Foujdar <jatin321001@gmail.com>
2024-07-31 12:02:19 +0530

    edit todo commit

--- todoList/src/component/Todo.jsx
+++ todoList/src/component/Todo.jsx
@@ -4,6 +4,8 @@ import { logo } from '../constant';
 const Todo = () => {
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState('');
+  const [editingTodo, setEditingTodo] = useState(null);
+  const [editingText, setEditingText] = useState('');
 
   const addTodo = () => {
     if (newTodo.trim() !== '') {
@@ -15,6 +17,11 @@ const Todo = () => {
   const deleteTodo = (id) => {
     setTodos(todos.filter(todo => todo.id !== id));
   };
+
+  const startEditing = (todo)=>{
+  setEditingTodo(todo);
+  setEditingText(todo.text);
+  }
 
   return (
     <div>
@@ -37,10 +44,34 @@ const Todo = () => {
       <ul>
         {todos.map((todo) => (
           <li key={todo.id} className="flex items-center">
+           {editingTodo && editingText.id === todo.id ? (
+            <>
+            <input
+                type="text"
+                value={editingText}
+                onChange={(e) => setEditingText(e.target.value)}
+                className="border p-2 mr-2 rounded flex-grow"
+              />
+              <button onClick={() => saveEdit(todo.id)} className="ml-2 bg-green-500 text-white p-1 rounded">
+                Save
+              </button>
+              <button onClick={cancelEdit} className="ml-2 bg-gray-500 text-white p-1 rounded">
+                Cancel
+              </button>
+            </>
+           ):(
+            <>
+            
             <span className="flex-grow">{todo.text}</span>
             <button onClick={() => deleteTodo(todo.id)} className="ml-2 bg-red-500 text-white p-1 rounded">
               Delete
             </button>
+            <button onClick={() => startEditing(todo)} className="ml-2 bg-red-500 text-white p-1 rounded">
+              Edit
+            </button>
+            </>
+           )}
+            
           </li>
         ))}
       </ul>
